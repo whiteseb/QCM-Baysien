7a741a6b0b34a89a24719730f82cf1197d8e1f53
whiteseb <81529188+whiteseb@users.noreply.github.com>
2024-11-30 20:42:54 +0100

    Update mini-quiz.js

--- mini-quiz.js
+++ mini-quiz.js
@@ -168,4 +168,3 @@ window.onload = function() {
     const nextButton = document.getElementById("next-button");
     nextButton.addEventListener("click", nextQuestion);  // Ajouter l'événement de clic sur le bouton
 };
-
